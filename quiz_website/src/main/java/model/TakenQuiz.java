d34ac12c62f9060a02e75a19defb556b90abd1c8
misho <Malkh19@freeuni.edu.ge>
2023-08-08 23:00:14 +0400

    added Takenquiz class

--- quiz_website/src/main/java/model/TakenQuiz.java
+++ quiz_website/src/main/java/model/TakenQuiz.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 public class TakenQuiz {
@@ -8,16 +9,27 @@ public class TakenQuiz {
     private Long quizId;
     private int score;
     private LocalDateTime attemptDate;
+    private Duration timeTaken;
+    private String feedback;
+    private String status;
+
+    // Default constructor
+    public TakenQuiz() {
+    }
 
     // Constructor
-    public TakenQuiz(Long takenQuizId, Long userId, Long quizId, int score, LocalDateTime attemptDate) {
+    public TakenQuiz(Long takenQuizId, Long userId, Long quizId, int score,
+                     LocalDateTime attemptDate, Duration timeTaken,
+                     String feedback, String status) {
         this.takenQuizId = takenQuizId;
         this.userId = userId;
         this.quizId = quizId;
         this.score = score;
         this.attemptDate = attemptDate;
+        this.timeTaken = timeTaken;
+        this.feedback = feedback;
+        this.status = status;
     }
-
 
     public Long getTakenQuizId() {
         return takenQuizId;
@@ -59,6 +71,34 @@ public class TakenQuiz {
         this.attemptDate = attemptDate;
     }
 
+    public boolean hasPassed(int passingScore) {
+        return this.score >= passingScore;
+    }
+
+    public Duration getTimeTaken() {
+        return timeTaken;
+    }
+
+    public void setTimeTaken(Duration timeTaken) {
+        this.timeTaken = timeTaken;
+    }
+
+    public String getFeedback() {
+        return feedback;
+    }
+
+    public void setFeedback(String feedback) {
+        this.feedback = feedback;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
     @Override
     public String toString() {
         return "TakenQuiz{" +
@@ -67,6 +107,9 @@ public class TakenQuiz {
                 ", quizId=" + quizId +
                 ", score=" + score +
                 ", attemptDate=" + attemptDate +
+                ", timeTaken=" + timeTaken +
+                ", feedback='" + feedback + '\'' +
+                ", status='" + status + '\'' +
                 '}';
     }
 }
