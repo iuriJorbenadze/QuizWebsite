bb9fb4a4143347425b40599932b641a788f0a13b
iuriJorbenadze <ijorb19@freeuni.edu.ge>
2023-08-09 18:12:49 +0400

    databasesetup fixed so on databse layer once user is deleted automatically conected table values are deleted, from quizes questions and so on, using cascade deleting one entity will make other entities which depend on it delete also and this will be handled on database level

--- quiz_website/src/main/java/database/DatabaseSetup.java
+++ quiz_website/src/main/java/database/DatabaseSetup.java
@@ -64,8 +64,6 @@ public class DatabaseSetup {
     }
 
 
-
-
     private void createUsersTable(Statement stmt) throws SQLException {
         String createUsersSQL = "CREATE TABLE IF NOT EXISTS Users(" +
                 "id INT AUTO_INCREMENT PRIMARY KEY," +
@@ -121,13 +119,11 @@ public class DatabaseSetup {
                 "timeTaken BIGINT," +  // storing duration as seconds
                 "feedback TEXT," +
                 "status VARCHAR(255)," +
-                "FOREIGN KEY(userId) REFERENCES Users(id)," +
-                "FOREIGN KEY(quizId) REFERENCES Quizzes(id)" +
+                "FOREIGN KEY(userId) REFERENCES Users(id) ON DELETE CASCADE," +
+                "FOREIGN KEY(quizId) REFERENCES Quizzes(id) ON DELETE CASCADE" +
                 ")";
         stmt.executeUpdate(createTakenQuizSQL);
     }
-
-
 
     private void createMessagesTable(Statement stmt) throws SQLException {
         String createMessagesSQL = "CREATE TABLE IF NOT EXISTS Messages(" +
@@ -159,15 +155,119 @@ public class DatabaseSetup {
                 "user1Id INT NOT NULL," +
                 "user2Id INT NOT NULL," +
                 "status ENUM('PENDING', 'ACCEPTED') NOT NULL DEFAULT 'PENDING'," +
-
                 "createdDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP," +
                 "acceptedDate TIMESTAMP," +   // This will be NULL initially
                 "FOREIGN KEY(user1Id) REFERENCES Users(id)," +
-                "FOREIGN KEY(user2Id) REFERENCES Users(id)" +  //was "," at the end
-
+                "FOREIGN KEY(user2Id) REFERENCES Users(id)" +
                 ")";
         stmt.executeUpdate(createFriendsSQL);
     }
+
+
+
+
+//    private void createUsersTable(Statement stmt) throws SQLException {
+//        String createUsersSQL = "CREATE TABLE IF NOT EXISTS Users(" +
+//                "id INT AUTO_INCREMENT PRIMARY KEY," +
+//                "username VARCHAR(255) NOT NULL UNIQUE," +
+//                "passwordHash VARCHAR(255) NOT NULL," +
+//                "email VARCHAR(255) NOT NULL UNIQUE," +
+//                "dateRegistered TIMESTAMP DEFAULT CURRENT_TIMESTAMP," +
+//                "isAdmin BOOLEAN DEFAULT FALSE" +
+//                ")";
+//        stmt.executeUpdate(createUsersSQL);
+//    }
+
+//    private void createQuizTable(Statement stmt) throws SQLException {
+//        String createQuizSQL = "CREATE TABLE IF NOT EXISTS Quizzes(" +
+//                "id INT AUTO_INCREMENT PRIMARY KEY," +
+//                "title VARCHAR(255) NOT NULL," +
+//                "description TEXT," +
+//                "createdByUserId INT," +
+//                "createdDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP," +
+//                "FOREIGN KEY(createdByUserId) REFERENCES Users(id)" +
+//                ")";
+//        stmt.executeUpdate(createQuizSQL);
+//    }
+//
+//    private void createQuestionsTable(Statement stmt) throws SQLException {
+//        String createQuestionsSQL = "CREATE TABLE IF NOT EXISTS Questions(" +
+//                "id INT AUTO_INCREMENT PRIMARY KEY," +
+//                "quizId INT NOT NULL," +
+//                "content TEXT NOT NULL," +
+//                "correctOptionId INT," +
+//                "FOREIGN KEY(quizId) REFERENCES Quizzes(id)" +
+//                ")";
+//        stmt.executeUpdate(createQuestionsSQL);
+//    }
+//
+//    private void createOptionsTable(Statement stmt) throws SQLException {
+//        String createOptionsSQL = "CREATE TABLE IF NOT EXISTS Options(" +
+//                "id INT AUTO_INCREMENT PRIMARY KEY," +
+//                "questionId INT NOT NULL," +
+//                "optionText TEXT NOT NULL," +
+//                "FOREIGN KEY(questionId) REFERENCES Questions(id)" +
+//                ")";
+//        stmt.executeUpdate(createOptionsSQL);
+//    }
+//
+//    private void createTakenQuizTable(Statement stmt) throws SQLException {
+//        String createTakenQuizSQL = "CREATE TABLE IF NOT EXISTS TakenQuiz(" +
+//                "id INT AUTO_INCREMENT PRIMARY KEY," +
+//                "userId INT NOT NULL," +
+//                "quizId INT NOT NULL," +
+//                "score INT NOT NULL," +
+//                "dateTaken TIMESTAMP DEFAULT CURRENT_TIMESTAMP," +
+//                "timeTaken BIGINT," +  // storing duration as seconds
+//                "feedback TEXT," +
+//                "status VARCHAR(255)," +
+//                "FOREIGN KEY(userId) REFERENCES Users(id)," +
+//                "FOREIGN KEY(quizId) REFERENCES Quizzes(id)" +
+//                ")";
+//        stmt.executeUpdate(createTakenQuizSQL);
+//    }
+//
+//
+//
+//    private void createMessagesTable(Statement stmt) throws SQLException {
+//        String createMessagesSQL = "CREATE TABLE IF NOT EXISTS Messages(" +
+//                "id INT AUTO_INCREMENT PRIMARY KEY," +
+//                "senderId INT NOT NULL," +
+//                "receiverId INT NOT NULL," +
+//                "messageText TEXT NOT NULL," +
+//                "dateSent TIMESTAMP DEFAULT CURRENT_TIMESTAMP," +
+//                "FOREIGN KEY(senderId) REFERENCES Users(id)," +
+//                "FOREIGN KEY(receiverId) REFERENCES Users(id)" +
+//                ")";
+//        stmt.executeUpdate(createMessagesSQL);
+//    }
+//
+//    private void createAchievementsTable(Statement stmt) throws SQLException {
+//        String createAchievementsSQL = "CREATE TABLE IF NOT EXISTS Achievements(" +
+//                "id INT AUTO_INCREMENT PRIMARY KEY," +
+//                "userId INT NOT NULL," +
+//                "achievementName VARCHAR(255) NOT NULL," +
+//                "dateEarned TIMESTAMP DEFAULT CURRENT_TIMESTAMP," +
+//                "FOREIGN KEY(userId) REFERENCES Users(id)" +
+//                ")";
+//        stmt.executeUpdate(createAchievementsSQL);
+//    }
+//
+//    private void createFriendsTable(Statement stmt) throws SQLException {
+//        String createFriendsSQL = "CREATE TABLE IF NOT EXISTS Friends(" +
+//                "id INT AUTO_INCREMENT PRIMARY KEY," +
+//                "user1Id INT NOT NULL," +
+//                "user2Id INT NOT NULL," +
+//                "status ENUM('PENDING', 'ACCEPTED') NOT NULL DEFAULT 'PENDING'," +
+//
+//                "createdDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP," +
+//                "acceptedDate TIMESTAMP," +   // This will be NULL initially
+//                "FOREIGN KEY(user1Id) REFERENCES Users(id)," +
+//                "FOREIGN KEY(user2Id) REFERENCES Users(id)" +  //was "," at the end
+//
+//                ")";
+//        stmt.executeUpdate(createFriendsSQL);
+//    }
     //"UNIQUE(user1Id, user2Id)" +  // Ensure unique combination
 
     public static void main(String[] args) {
